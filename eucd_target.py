39bad58fc0cd28b147fd9c659799e766ce7e689f
Mkanobi <maksymilian_piotrowski@wp.pl>
2022-03-10 14:28:49 +0100

    Wczytywanie i funkcja celu dla EUC_2D
    Zaimplementowano wczytywanie oraz funkcję celu dla EUC_2D

---
+++ eucd_target.py
@@ -0,0 +1,11 @@
+import tsplib95
+#EUC_2D
+problem = tsplib95.load('a280.tsp')
+
+def target_function_euc_2d(problem, path):
+    solution=0
+    for i in range(1,len(path)):
+        solution += problem.get_weight(path[i-1],path[i])
+    return solution
+
+print(target_function_euc_2d(problem, [1,2,3]))
