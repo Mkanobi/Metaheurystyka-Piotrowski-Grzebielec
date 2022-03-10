737105bba235fed4882b6e177a3fc2828acf604c
jakgrz <grzebielec.jakub@gmail.com>
2022-03-10 15:50:37 +0100

    problemRender for all tsp

--- TSP.py
+++ TSP.py
@@ -2,12 +2,34 @@ import random
 import tsplib95
 
 
-def problemRender(name, dim, rf, rb):
+def problemRender_euclidean(name, dim, rf, rb):
     prob = f'NAME: {name}\nTYPE: TSP\nDIMENSION: {dim}\nEDGE_WEIGHT_TYPE: EUC_2D\nNODE_COORD_SECTION:\n'
     for i in range(1, dim + 1):
         prob += f'{i} {random.randint(rf, rb)} {random.randint(rf, rb)}\n'
     return prob
 
 
-problem = tsplib95.parse(problemRender('test', 8, 0, 100))
+def problemRender_symmetrical(name, dim, rf, rb):
+    prob = f'NAME: {name}\nTYPE: TSP\nDIMENSION: {dim}\nEDGE_WEIGHT_TYPE: EXPLICIT\nEDGE_WEIGHT_FORMAT: UPPER_ROW\nDISPLAY_DATA_TYPE: NO_DISPLAY\nEDGE_WEIGHT_SECTION\n'
+    for i in range(dim - 1, 0, -1):
+        for j in range(0, i):
+            prob += f'{random.randint(rf, rb)} '
+        prob += '\n'
+    return prob
+
+
+def problemRender_asymmetrical(name, dim, rf, rb):
+    prob = f'NAME: {name}\nTYPE: ATSP\nDIMENSION: {dim}\nEDGE_WEIGHT_TYPE: EXPLICIT\nEDGE_WEIGHT_FORMAT: FULL_MATRIX\nDISPLAY_DATA_TYPE: NO_DISPLAY\nEDGE_WEIGHT_SECTION\n'
+    for i in range(0, dim):
+        for j in range(0, dim):
+            if i == j:
+                prob += '9999'
+            else:
+                prob += f'{random.randint(rf, rb)}'
+            prob += ' '
+        prob += '\n'
+    return prob
+
+
+problem = tsplib95.parse(problemRender_asymmetrical('test', 7, 0, 10))
 print(problem.render())
