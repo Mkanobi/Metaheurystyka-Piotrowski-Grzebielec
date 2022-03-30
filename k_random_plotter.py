72689a242ed6ec85263f3c95ac0909c49c11e8bf
Mkanobi <maksymilian_piotrowski@wp.pl>
2022-03-30 15:29:00 +0200

    Dodano wykresy rozwiązań k-random.

--- k_random_plotter.py
+++ k_random_plotter.py
@@ -1,19 +1,40 @@
 import matplotlib.pyplot as plt
+from algorithms import extended_nearest_neighbour, nearest_neighbour, two_opt
 from k_random_testing import tester_k_random
-import numpy
+from problem_render import problem_render_asymmetrical, problem_render_euclidean, problem_render_symmetrical
+import tsplib95
 
-x = [i for i in range(1, 101, 1)]
-npx = numpy.array(x)
-results = tester_k_random(100,x)
+x = [i for i in range(1, 501, 1)]
+problem_size = 100
+number_of_tries  = 10
+problem_type = "EUC_2D"
 
-# print(numpy.array(results))
+if problem_type == "EUC_2D":
+    problem1 = tsplib95.parse(problem_render_euclidean('problem testowy',problem_size,0,500))
+elif problem_type == "TSP_EXPLICIT":
+    problem1 = tsplib95.parse(problem_render_symmetrical('problem testowy',problem_size,0,500))
+elif problem_type == "ATSP_EXPLICIT":
+    problem1 = tsplib95.parse(problem_render_asymmetrical('problem testowy',problem_size,0,500))
 
-# plt.subplot(1, 2, 1)
+two_solution = two_opt(problem1)
+nearest_solution = nearest_neighbour(problem1,1)
+nearest_extended_solution = extended_nearest_neighbour(problem1)
+print(nearest_solution)
+print(nearest_extended_solution)
+print(two_solution)
+y_2opt = [two_solution for i in range(len(x))]
+y_nearest = [nearest_solution for i in range(len(x))]
+y_extended_nearest = [nearest_extended_solution for i in range(len(x))]
+results = tester_k_random(number_of_tries,x,problem1)
+
+
 plt.plot(x, results, "-r", label="k-random")
-# plt.plot(npx, [numpy.nan for _ in range(5)], "-c", label="square")
-plt.legend(loc="upper left")
-plt.title("Wykres dla problemu o wielkosci 100 w zależności od k dla k-random")
+plt.plot(x, y_nearest, "-g", label = "nearest neighbour")
+plt.plot(x, y_extended_nearest, "-m", label = "extended nearest neighbour")
+plt.plot(x, y_2opt, "-b", label = "2-opt")
+plt.title("Wykres średnich z " + str(number_of_tries) + " rozwiązań k-random dla wygenerowanego problemu " + str(problem_type) + " o DIMENSION " + str(problem_size) + " w zależności od k")
 plt.grid()
-
-
+plt.xlabel("Wielkość k dla k-random")
+plt.ylabel("Średnia długość ścieżki znalezionej przez k-random")
+plt.legend(loc="upper right")
 plt.show()
--- k_random_testing.py
+++ k_random_testing.py
@@ -1,19 +1,17 @@
-import tsplib95
-import time
 from algorithms import k_random
-from problem_render import problem_render_euclidean
 import statistics as stat
-import numpy
+import sys
 
-def tester_k_random(k, x):
-    results = [0 for _ in range(len(x))]
-    tmp = [[0 for _ in range(k)] for _ in range(len(x))]
+def tester_k_random(number_of_tries_for_each_k, ktab, problem):
+    results = [0 for _ in range(len(ktab))]
+    tmp = [[0 for _ in range(number_of_tries_for_each_k)] for _ in range(len(ktab))]
 
-    for m in range(len(x)):
-        for j in range(k):
-            problem1 = tsplib95.parse(problem_render_euclidean('problem testowy',100,0,500))
-            tmp[m][j] = k_random(problem1,x[m])
-            
-        for l in range(len(x)):
+    for m in range(len(ktab)):
+        print(ktab[m], end=" ")
+        sys.stdout.flush()
+        for j in range(number_of_tries_for_each_k):
+            tmp[m][j] = k_random(problem,ktab[m])
+
+        for l in range(len(ktab)):
             results[l] = stat.mean(tmp[l])
     return results
