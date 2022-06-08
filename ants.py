d7873e23ec4564f92e30af6a2c28f50f48b18b52
jakgrz <grzebielec.jakub@gmail.com>
2022-06-08 22:20:44 +0200

    nie wiem czy warto

--- ants.py
+++ ants.py
@@ -1,18 +1,28 @@
+"""
+Ta funkcja teoretycznie działa
+Wykorzystane procesy
+Część paralelizacji została poświęcona
+Brak profitu
+"""
+
+
+
 from goal_function import goal_function as goal
 from random import choices, randrange, shuffle
 from threading import Thread
+from multiprocessing import Pool
 import math
 
 # from StackOverflow
 def split(a, n):
     k, m = divmod(len(a), n)
     return (a[i*k+min(i, m):(i+1)*k+min(i+1, m)] for i in range(n))
+#
+# def ant_walk_mult(weights: list, pheromones: list, starting_city: list, alpha: int, beta: int, added: list, result: list) -> None:
+#     for str_ct in starting_city:
+#         ant_walk(weights, pheromones, str_ct, alpha, beta, added, result)
 
-def ant_walk_mult(weights: list, pheromones: list, starting_city: list, alpha: int, beta: int, added: list, result: list) -> None:
-    for str_ct in starting_city:
-        ant_walk(weights, pheromones, str_ct, alpha, beta, added, result)
-
-def ant_walk(weights: list, pheromones: list, starting_city: int, alpha: int, beta: int, added: list, result: list) -> None:
+def ant_walk(arguments) -> None:
     """
     weights: 2D list of edge weights
     pheromones: 2D list of pheromones deposit
@@ -22,6 +32,12 @@ def ant_walk(weights: list, pheromones: list, starting_city: int, alpha: int, be
     Q: The ants pheromone stack
     added, result: Return lists for threads operations
     """
+    # print('xd')
+    weights = arguments[0]
+    pheromones = arguments[1]
+    starting_city = arguments[2]
+    alpha = arguments[3]
+    beta = arguments[4]
     dim = len(weights)
     path = [starting_city]
     path_len = 0
@@ -43,11 +59,11 @@ def ant_walk(weights: list, pheromones: list, starting_city: int, alpha: int, be
     path_len += weights[path[-1]][starting_city]
 
     # Pheromones update
-    delta = 100 / path_len
-    for idx in range(dim):
-        added[path[idx]][path[(idx+1) % dim]].append(delta)
+    # delta = 100 / path_len
+    # for idx in range(dim):
+    #     added[path[idx]][path[(idx+1) % dim]].append(delta)
 
-    result.append([path, path_len])
+    return [path, path_len]
 
 def ant_colony(problem, iterations: int, colony_size: int, t_cnt: int, alpha: int, beta: int, rho: int) -> list:
     result = [[], math.inf]
@@ -58,24 +74,29 @@ def ant_colony(problem, iterations: int, colony_size: int, t_cnt: int, alpha: in
     pheromones = [[100.0 for _ in range(dim)] for _ in range(dim)]
 
     for itr in range(iterations):
-        added = [[[] for _ in range(dim)] for _ in range(dim)]
         threads = []
-        paths = []
         tmp = list(range(dim))
         shuffle(tmp)
         tmp = tmp[:colony_size]
-        starting_points = list(split(tmp, t_cnt))
-        for starting_city in starting_points:
-            threads.append(Thread(
-                target=ant_walk_mult,
-                args=[weights, pheromones, starting_city, alpha, beta, added, paths]))
-        for t in threads: t.start()
-        for t in threads: t.join()
+        arguments = [[weights, pheromones, i, alpha, beta] for i in tmp]
+        # starting_points = list(split(tmp, t_cnt))
+        # for starting_city in starting_points:
+        #     threads.append(Process(
+        #         target=ant_walk_mult,
+        #         args=(weights, pheromones, starting_city, alpha, beta, added, paths)))
+        # for t in threads: t.start()
+        # for t in threads: t.join()
+        with Pool(processes=t_cnt) as pool:
+            paths = pool.map(ant_walk, arguments)
+
+        for path in paths:
+            delta = 100 / path[1]
+            for idx in range(dim):
+                pheromones[path[0][idx]][path[0][(idx+1) % dim]] += delta
 
         for i in range(dim):
             for j in range(dim):
                 pheromones[i][j] *= 1-rho
-                pheromones[i][j] += sum(added[i][j])
 
         for path in paths:
             if path[1] < result[1]: result = path[:]
--- run_algorithms.py
+++ run_algorithms.py
@@ -13,168 +13,169 @@ from elite_ants import ant_colony as elite
 from elite_ants_timed import ant_colony as timed
 from elite_ants_timed_randomized import ant_colony as elite_timed_randomized
 
-parser = argparse.ArgumentParser()
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
 
-parser.add_argument('--type', type=str,
-                    help='Typ problemu do wygenerowania (EUC_2D, ATSP_EXPLICIT, TSP_EXPLICIT)')
-parser.add_argument('--dim', type=int,
-                    help='Dimension problemu do wygenerowania')
-parser.add_argument('--min', type=int,
-                    help='Min value odległości/pozycji problemu do wygenerowania')
-parser.add_argument('--max', type=int,
-                    help='Max value odległości/pozycji problemu do wygenerowania')
-parser.add_argument('--input', type=str,
-                    help='Nazwa pliku do użycia jako input')
-parser.add_argument('--output', type=str,
-                    help='Nazwa pliku do użycia jako output wygenerowanego problemu')
-
-
-type = "EUC_2D"
-dim = 100
-minimum = 0
-maximum = 500
-args = parser.parse_args()
-if (args.type is not None):
-    if (args.type != "EUC_2D" and args.type != "ATSP_EXPLICIT" and args.type != "TSP_EXPLICIT"):
-        parser.error("Nieznany typ problemu do wygenerowania! (podaj EUC_2D, ATSP_EXPLICIT, TSP_EXPLICIT)")
-    type = args.type
-if (args.dim is not None):
-    dim = args.dim
-if (args.min is not None):
-    minimum = args.min
-if (args.max is not None):
-    maximum = args.max
-
-if (args.input is None):
-    if (type == "EUC_2D"):
-        problem = tsplib95.parse(problem_render_euclidean('problem testowy',dim,minimum,maximum))
-    if (type == "TSP_EXPLICIT"):
-        problem = tsplib95.parse(problem_render_symmetrical('problem testowy',dim,minimum,maximum))
-    if (type == "ATSP_EXPLICIT"):
-        problem = tsplib95.parse(problem_render_asymmetrical('problem testowy',dim,minimum,maximum))
-else:
-    problem = tsplib95.load(args.input)
-    type = problem.type + "_" + problem.edge_weight_type
-    dim = problem.dimension
-
-if (args.output is not None):
-    f = open(args.output, "a")
-    f.write(problem.render())
-    f.close()
-
-print('Typ problemu: ' + type + ', ' + 'Rozmiar: ' + str(dim) + '\n')
-
-result = [math.inf for _ in range(11)]
-exec_time = [0.0 for _ in range(11)]
-
-start = time.time()
-tmp = k_random(problem,1000)
-result[0] = tmp[1]
-end = time.time()
-exec_time[0] = end - start
-print("Obliczono k-random...")
-
-start = time.time()
-tmp1 = nearest_neighbour(problem,1)
-result[1] = tmp1[1]
-end = time.time()
-exec_time[1] = end - start
-print("Obliczono nn...")
-
-start = time.time()
-result[2] = extended_nearest_neighbour(problem)[1]
-end = time.time()
-exec_time[2] = end - start
-print("Obliczono enn...")
-
-start = time.time()
-result[3] = two_opt(problem)[1]
-end = time.time()
-exec_time[3] = end - start
-print("Obliczono two_opt...")
-
-start = time.time()
-result[4] = tabu_search(problem,tmp1[0],'swap',7,3,100,10)[1]
-end = time.time()
-exec_time[4] = end - start
-print("Obliczono tabu_search...")
-
-exe = tabu_time(problem,tmp1[0],'swap',7,3,5,10)
-result[5] = exe[1]
-exec_time[5] = exe[2]
-print("Obliczono tabu_time...")
-
-start = time.time()
-result[6] = ant_colony(problem, 40, 20, 4, 1, 3, 0.1)[1]
-end = time.time()
-exec_time[6] = end - start
-print('Obliczono ACO...')
-
-start = time.time()
-result[7] = tourney(problem, 40, 20, 4, 1, 3, 0.1, 7)[1]
-end = time.time()
-exec_time[7] = end - start
-print('Obliczono Tourney...')
-
-start = time.time()
-result[8] = elite(problem, 40, 20, 4, 1, 3, 0.1, 0.2)[1]
-end = time.time()
-exec_time[8] = end - start
-print('Obliczono Elite...')
-
-exe = timed(problem, 5, 20, 4, 1, 3, 0.1, 0.2)
-result[9] = exe[1]
-exec_time[9] = exe[2]
-print('Obliczono Timed...\n')
-
-exe = elite_timed_randomized(problem, 5, 20, 4, 1, 3, 0.1, 0, 0)
-result[10] = exe[1]
-exec_time[10] = exe[2]
-print('Obliczono Timed, elite, randomized...\n')
-
-best = min(result)
+    parser.add_argument('--type', type=str,
+                        help='Typ problemu do wygenerowania (EUC_2D, ATSP_EXPLICIT, TSP_EXPLICIT)')
+    parser.add_argument('--dim', type=int,
+                        help='Dimension problemu do wygenerowania')
+    parser.add_argument('--min', type=int,
+                        help='Min value odległości/pozycji problemu do wygenerowania')
+    parser.add_argument('--max', type=int,
+                        help='Max value odległości/pozycji problemu do wygenerowania')
+    parser.add_argument('--input', type=str,
+                        help='Nazwa pliku do użycia jako input')
+    parser.add_argument('--output', type=str,
+                        help='Nazwa pliku do użycia jako output wygenerowanego problemu')
 
 
-print('Rozwiazanie 1000-Random: ' + str(result[0]))
-print('PRD 1000-Random: ' + "{:.2%}".format( (result[0]-best)/best) )
-print('Czas 1000-Random: ' + str(exec_time[0]) + ' s\n')
+    type = "EUC_2D"
+    dim = 100
+    minimum = 0
+    maximum = 500
+    args = parser.parse_args()
+    if (args.type is not None):
+        if (args.type != "EUC_2D" and args.type != "ATSP_EXPLICIT" and args.type != "TSP_EXPLICIT"):
+            parser.error("Nieznany typ problemu do wygenerowania! (podaj EUC_2D, ATSP_EXPLICIT, TSP_EXPLICIT)")
+        type = args.type
+    if (args.dim is not None):
+        dim = args.dim
+    if (args.min is not None):
+        minimum = args.min
+    if (args.max is not None):
+        maximum = args.max
 
-print('Rozwiazanie Nearest Nieghbour: ' + str(result[1]))
-print('PRD Nearest Nieghbour: ' + "{:.2%}".format( (result[1]-best)/best))
-print('Czas Nearest Nieghbour: ' + str(exec_time[1]) + ' s\n')
+    if (args.input is None):
+        if (type == "EUC_2D"):
+            problem = tsplib95.parse(problem_render_euclidean('problem testowy',dim,minimum,maximum))
+        if (type == "TSP_EXPLICIT"):
+            problem = tsplib95.parse(problem_render_symmetrical('problem testowy',dim,minimum,maximum))
+        if (type == "ATSP_EXPLICIT"):
+            problem = tsplib95.parse(problem_render_asymmetrical('problem testowy',dim,minimum,maximum))
+    else:
+        problem = tsplib95.load(args.input)
+        type = problem.type + "_" + problem.edge_weight_type
+        dim = problem.dimension
 
-print('Rozwiazanie Extended Nearest Nieghbour: ' + str(result[2]))
-print('PRD Extended Nearest Nieghbour: ' + "{:.2%}".format( (result[2]-best)/best))
-print('Czas Extended Nearest Nieghbour: ' + str(exec_time[2]) + ' s\n')
+    if (args.output is not None):
+        f = open(args.output, "a")
+        f.write(problem.render())
+        f.close()
 
-print('Rozwiazanie 2-OPT: ' + str(result[3]))
-print('PRD 2-OPT: ' + "{:.2%}".format( (result[3]-best)/best))
-print('Czas 2-OPT: ' + str(exec_time[3]) + ' s\n')
+    print('Typ problemu: ' + type + ', ' + 'Rozmiar: ' + str(dim) + '\n')
 
-print('Rozwiazanie tabu: ' + str(result[4]))
-print('PRD tabu: ' + "{:.2%}".format( (result[4]-best)/best))
-print('Czas tabu: ' + str(exec_time[4]) + ' s\n')
+    result = [math.inf for _ in range(11)]
+    exec_time = [0.0 for _ in range(11)]
 
-print('Rozwiazanie tabu_time: ' + str(result[5]))
-print('PRD tabu_time: ' + "{:.2%}".format( (result[5]-best)/best))
-print('Czas tabu_time: ' + str(exec_time[5]) + ' s\n')
+    start = time.time()
+    tmp = k_random(problem,1000)
+    result[0] = tmp[1]
+    end = time.time()
+    exec_time[0] = end - start
+    print("Obliczono k-random...")
 
-print('Rozwiazanie ACO: ' + str(result[6]))
-print('PRD ACO: ' + "{:.2%}".format( (result[6]-best)/best))
-print('Czas ACO: ' + str(exec_time[6]) + ' s\n')
+    start = time.time()
+    tmp1 = nearest_neighbour(problem,1)
+    result[1] = tmp1[1]
+    end = time.time()
+    exec_time[1] = end - start
+    print("Obliczono nn...")
 
-print('Rozwiazanie Tourney: ' + str(result[7]))
-print('PRD Tourney: ' + "{:.2%}".format( (result[7]-best)/best))
-print('Czas Tourney: ' + str(exec_time[7]) + ' s\n')
+    start = time.time()
+    result[2] = extended_nearest_neighbour(problem)[1]
+    end = time.time()
+    exec_time[2] = end - start
+    print("Obliczono enn...")
 
-print('Rozwiazanie Elite: ' + str(result[8]))
-print('PRD Elite: ' + "{:.2%}".format( (result[8]-best)/best))
-print('Czas Elite: ' + str(exec_time[8]) + ' s\n')
+    start = time.time()
+    result[3] = two_opt(problem)[1]
+    end = time.time()
+    exec_time[3] = end - start
+    print("Obliczono two_opt...")
 
-print('Rozwiazanie Timed: ' + str(result[9]))
-print('PRD Timed: ' + "{:.2%}".format( (result[9]-best)/best))
-print('Czas Timed: ' + str(exec_time[9]) + ' s\n')
+    start = time.time()
+    result[4] = tabu_search(problem,tmp1[0],'swap',7,3,100,10)[1]
+    end = time.time()
+    exec_time[4] = end - start
+    print("Obliczono tabu_search...")
 
-print('Rozwiazanie Elite Timed Randomized: ' + str(result[10]))
-print('PRD Timed: ' + "{:.2%}".format( (result[10]-best)/best))
-print('Czas Timed: ' + str(exec_time[10]) + ' s\n')
+    exe = tabu_time(problem,tmp1[0],'swap',7,3,5,10)
+    result[5] = exe[1]
+    exec_time[5] = exe[2]
+    print("Obliczono tabu_time...")
+
+    start = time.time()
+    result[6] = ant_colony(problem, 40, 20, 4, 1, 3, 0.1)[1]
+    end = time.time()
+    exec_time[6] = end - start
+    print('Obliczono ACO...')
+
+    start = time.time()
+    result[7] = tourney(problem, 40, 20, 4, 1, 3, 0.1, 7)[1]
+    end = time.time()
+    exec_time[7] = end - start
+    print('Obliczono Tourney...')
+
+    start = time.time()
+    result[8] = elite(problem, 40, 20, 4, 1, 3, 0.1, 0.2)[1]
+    end = time.time()
+    exec_time[8] = end - start
+    print('Obliczono Elite...')
+
+    exe = timed(problem, 5, 20, 4, 1, 3, 0.1, 0.2)
+    result[9] = exe[1]
+    exec_time[9] = exe[2]
+    print('Obliczono Timed...\n')
+
+    exe = elite_timed_randomized(problem, 5, 20, 4, 1, 3, 0.1, 0, 0)
+    result[10] = exe[1]
+    exec_time[10] = exe[2]
+    print('Obliczono Timed, elite, randomized...\n')
+
+    best = min(result)
+
+
+    print('Rozwiazanie 1000-Random: ' + str(result[0]))
+    print('PRD 1000-Random: ' + "{:.2%}".format( (result[0]-best)/best) )
+    print('Czas 1000-Random: ' + str(exec_time[0]) + ' s\n')
+
+    print('Rozwiazanie Nearest Nieghbour: ' + str(result[1]))
+    print('PRD Nearest Nieghbour: ' + "{:.2%}".format( (result[1]-best)/best))
+    print('Czas Nearest Nieghbour: ' + str(exec_time[1]) + ' s\n')
+
+    print('Rozwiazanie Extended Nearest Nieghbour: ' + str(result[2]))
+    print('PRD Extended Nearest Nieghbour: ' + "{:.2%}".format( (result[2]-best)/best))
+    print('Czas Extended Nearest Nieghbour: ' + str(exec_time[2]) + ' s\n')
+
+    print('Rozwiazanie 2-OPT: ' + str(result[3]))
+    print('PRD 2-OPT: ' + "{:.2%}".format( (result[3]-best)/best))
+    print('Czas 2-OPT: ' + str(exec_time[3]) + ' s\n')
+
+    print('Rozwiazanie tabu: ' + str(result[4]))
+    print('PRD tabu: ' + "{:.2%}".format( (result[4]-best)/best))
+    print('Czas tabu: ' + str(exec_time[4]) + ' s\n')
+
+    print('Rozwiazanie tabu_time: ' + str(result[5]))
+    print('PRD tabu_time: ' + "{:.2%}".format( (result[5]-best)/best))
+    print('Czas tabu_time: ' + str(exec_time[5]) + ' s\n')
+
+    print('Rozwiazanie ACO: ' + str(result[6]))
+    print('PRD ACO: ' + "{:.2%}".format( (result[6]-best)/best))
+    print('Czas ACO: ' + str(exec_time[6]) + ' s\n')
+
+    print('Rozwiazanie Tourney: ' + str(result[7]))
+    print('PRD Tourney: ' + "{:.2%}".format( (result[7]-best)/best))
+    print('Czas Tourney: ' + str(exec_time[7]) + ' s\n')
+
+    print('Rozwiazanie Elite: ' + str(result[8]))
+    print('PRD Elite: ' + "{:.2%}".format( (result[8]-best)/best))
+    print('Czas Elite: ' + str(exec_time[8]) + ' s\n')
+
+    print('Rozwiazanie Timed: ' + str(result[9]))
+    print('PRD Timed: ' + "{:.2%}".format( (result[9]-best)/best))
+    print('Czas Timed: ' + str(exec_time[9]) + ' s\n')
+
+    print('Rozwiazanie Elite Timed Randomized: ' + str(result[10]))
+    print('PRD Timed: ' + "{:.2%}".format( (result[10]-best)/best))
+    print('Czas Timed: ' + str(exec_time[10]) + ' s\n')
